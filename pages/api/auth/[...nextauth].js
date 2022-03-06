63bc1d69f5a11e806c5b4a2dde5bf80fc8bbaf2c
Iput <taufiqurrohman.suwarto@gmail.com>
2022-03-06 09:02:01 +0700

    add master

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,46 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const clientSecret = process.env.MASTER_SECRET;
+const clientId = process.env.MASTER_ID;
+const wellKnown = process.env.MASTER_WELL_KNOWN;
+const scope = process.env.MASTER_SCOPE;
+
 export default NextAuth({
   // Configure one or more authentication providers
   pages: {
     signIn: "/seleksi-jpt/signin",
   },
   providers: [
+    {
+      name: "SIMASTER",
+      id: "seleksi-jpt",
+      type: "oauth",
+      wellKnown,
+      clientId,
+      clientSecret,
+      authorization: {
+        params: {
+          scope,
+          prompt: "login",
+        },
+      },
+      idToken: true,
+      checks: ["pkce", "state"],
+      profile(profile, token) {
+        const currentUser = {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          employee_number: employee_number || "",
+          role,
+          group,
+        };
+
+        return currentUser;
+      },
+    },
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
