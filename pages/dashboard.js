25c760da2e82b2c4c8a42e9df232b05e3c0ec487
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-03-11 18:31:23 +0700

    add ifles

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,66 @@
 import { PrinterOutlined } from "@ant-design/icons";
-import { Alert, Button, Card, Divider, message } from "antd";
+import { Alert, Button, Card, DatePicker, Divider, Form, message } from "antd";
 import FileSaver from "file-saver";
+import Moment from "moment";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import services from "../services";
 import Layout from "../src/components/Layout";
 
-const ResumeTerkirim = () => {
+const FormTMTPengangkatanPertama = ({ initialValues }) => {
+  const queryClient = useQueryClient();
+  const updateMutation = useMutation((data) => services.updateResume(data), {
+    onSuccess: () => {
+      message.success("Data perbaikan pengangkatan pertama berhasil disimpan");
+      queryClient.invalidateQueries("resume");
+    },
+    onError: () => {
+      message.error("Data gagal disimpan");
+    },
+  });
+
+  const handleSubmit = (data) => {
+    const currentValues = {
+      tmt_pengangkatan_pertama: data["tmt_pengangkatan_pertama"]
+        ? data["tmt_pengangkatan_pertama"]
+        : null,
+    };
+
+    updateMutation.mutateAsync(currentValues);
+  };
+  const [form] = Form.useForm();
+
+  return (
+    <>
+      <Alert
+        type="warning"
+        message="Perhatian"
+        description="Silahkan merubah data untuk TMT Pengangkatan pertama jika tmt pengangkatan pertama pada cetak masih kosong"
+      />
+      <Form
+        name="update-data"
+        initialValues={initialValues}
+        form={form}
+        onFinish={handleSubmit}
+      >
+        <Form.Item
+          name="tmt_pengangkatan_pertama"
+          extra="TMT Pengangkatan Pertama dalam JPTP"
+          label="TMT Pengangkatan Pertama"
+        >
+          <DatePicker format={"DD-MM-YYYY"} />
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit">Rubah</Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+const ResumeTerkirim = ({ dataResume }) => {
   const [loading, setLoading] = useState(false);
 
   const handleCetak = async () => {
@@ -38,6 +90,14 @@ const ResumeTerkirim = () => {
         showIcon
       />
       <Divider />
+      <FormTMTPengangkatanPertama
+        initialValues={{
+          tmt_pengangkatan_pertama: dataResume?.tmt_pengangkatan_pertama
+            ? Moment(dataResume?.tmt_pengangkatan_pertama)
+            : "",
+        }}
+      />
+
       <Button
         icon={<PrinterOutlined />}
         loading={loading}
@@ -76,7 +136,7 @@ const Dashboard = () => {
           pendaftaran seleksi JPT Madya di Pemerintah Provinsi Jawa Timur.
         </div>
         {dataResume && dataResume?.is_submit ? (
-          <ResumeTerkirim />
+          <ResumeTerkirim dataResume={dataResume} />
         ) : (
           <ResumeTakTerkirim />
         )}
